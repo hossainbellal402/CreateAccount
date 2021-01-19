0aa79f873f5fd6f4f27241f8389e4516eeb24a2c
hossainbellal402 <66582075+hossainbellal402@users.noreply.github.com>
2021-01-19 18:55:34 -0500

    add all code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import React from "react";
-
+import Header from './Header.js';
+import{ BrowserRouter as Router,Route,switch } from "react-router-dom";
+  
 
 function App() {
   return (
     <div className="App">
        <h1>Leat't build new app ! </h1>
+
+       <Header/>
     </div>
   );
 }
---
+++ my-app/src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css';
+import { link} from 'react-router-dom';
+
+function Header() {
+    return (
+        <div className="header">
+            <link className= "link" to="/" >
+                <img src=" "/> 
+            </link>
+            <link className= "/register" to= "/register" >
+            <button className= "header_button" >Create New Account </button>
+            </link>
+        </div>
+    )
+}
+import './Header.css';
+export
+
