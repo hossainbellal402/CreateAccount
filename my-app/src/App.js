cae4ab76ca8a5151ac3b2f48031a4cdf94692476
hossainbellal402 <66582075+hossainbellal402@users.noreply.github.com>
2021-01-19 01:32:12 -0500

    this is my first app coding

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Leat't build new app ! </h1>
+    </div>
+  );
+}
+
+export default App;
