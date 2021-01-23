6c27523525663be431656eb4b8f18035d8c0c529
hossainbellal402 <66582075+hossainbellal402@users.noreply.github.com>
2021-01-23 02:26:02 -0500

    send all code

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './login.css';
-
+import {Link} from 'react-router-dom';
 
 function Login() {
     return (
         <div className="login">
             <div className="login_continer">
+                <h3>Log In To New App</h3>
                 <form>
                     <center>
                         <input type="text" placeholder="Email Address"/>
@@ -14,7 +15,19 @@ function Login() {
                         <input type="text" placeholder="Password"/>  
                     </center>
                     <center>
-                        <button type="submit" className="login_login">Log In </button>
+                        <button type="submit" className="login_login"> Log In </button>
+                    </center>
+                    <center>
+                        <h6>Forgotten Password</h6>
+                    </center>
+                    <center>
+                        <hr/>
+                    </center>
+                    <center>
+                        <link to="/register">
+                            <button className="login_CreateNewAccount">Create New Account</button>
+
+                        </link>
                     </center>
                 </form>
 
